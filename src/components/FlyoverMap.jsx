117d196d23d37c50f5f3cd736b885a20190d9ef6
Sandip nagar <sohamnagar0001@gmail.com>
2026-09-10 11:52:18 +0530

    change the width of basemap tab

--- src/components/FlyoverMap.jsx
+++ src/components/FlyoverMap.jsx
@@ -246,7 +246,7 @@ function BaseMapPanel({ open, setOpen, baseMap, onChange }) {
     );
   }
   return (
-    <div className="w-44 rounded-lg bg-white shadow-lg p-3 text-sm">
+    <div className="w-32 rounded-lg bg-white shadow-lg p-3 text-sm">
       <div className="flex items-center justify-between mb-2">
         <span className="font-semibold text-gray-800">Layers</span>
         <button
@@ -670,6 +670,7 @@ export default function FlyoverMap({
         zoomControl={true}
         touchZoom={true}
         attributionControl={false}
+        keyboard={false}
         style={{
           height: "100%",
           width: "100%",
